27d1815e2b53f57400ddc6a470440a3d0c8569aa
mcriqui <maggie.criqui@gmail.com>
2014-12-29 17:40:08 -0500

    Update beer.py

--- beer.py
+++ beer.py
@@ -1,2 +1,5 @@
 for number, number2 in zip(reversed(range(1,100)), reversed(range(0,99))):
-	print "{0} bottles of beer on the wall. {0} bottles of beer. Take one down, pass it around, {1} bottles of beer on the wall ".format(number, number2)
+	print """{0} bottles of beer on the wall. 
+		 {0} bottles of beer. 
+	         Take one down, pass it around, 
+	         {1} bottles of beer on the wall.""".format(number, number2)
