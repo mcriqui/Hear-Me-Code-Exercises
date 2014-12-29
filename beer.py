3c8ad54ba84a584601d8665b34045d9c1aed3ac0
mcriqui <maggie.criqui@gmail.com>
2014-12-29 17:39:12 -0500

    Create beer.py

---
+++ beer.py
@@ -0,0 +1,2 @@
+for number, number2 in zip(reversed(range(1,100)), reversed(range(0,99))):
+	print "{0} bottles of beer on the wall. {0} bottles of beer. Take one down, pass it around, {1} bottles of beer on the wall ".format(number, number2)
